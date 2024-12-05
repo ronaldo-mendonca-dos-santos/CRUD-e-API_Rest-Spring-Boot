880560b022558dd06087eb9338a8fa83dbc0e626
Ronaldo Mendonça dos Santos <ronaldomendoncadossantos@gmail.com>
2024-12-05 18:23:00 -0300

    CRUD + API Rest com Spring Boot

---
+++ src/main/java/com/ronaldo/crud/dtos/ProductDto.java
@@ -0,0 +1,4 @@
+package com.ronaldo.crud.dtos;
+
+public record ProductDto(String name, Long price) {
+}
---
+++ src/main/java/com/ronaldo/crud/model/Product.java
@@ -0,0 +1,48 @@
+package com.ronaldo.crud.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "product")
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters e Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
